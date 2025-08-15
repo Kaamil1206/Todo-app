4bcec0e62980c2b8d54ec88e4b2d2b85af3cb7a9
Kaamil1210 <kaamilgohel1210@gmail.com>
2025-08-15 08:55:40 +0530

    modified:   src/MyComponents/Header.js

--- src/MyComponents/Header.js
+++ src/MyComponents/Header.js
@@ -1,11 +1,18 @@
-
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 
 export default function Header(props) {
+    const [darkMode, setDarkMode] = useState(false);
+
+    const toggleTheme = () => {
+        setDarkMode(!darkMode);
+        document.body.className = darkMode ? "bg-light text-dark" : "bg-dark text-light";
+    };
+
     return (
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <nav className={`navbar navbar-expand-lg ${darkMode ? "navbar-dark bg-dark" : "navbar-light bg-light"}`}>
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">{props.title}</a>
+                <a className="navbar-brand fw-bold" href="#">{props.title}</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
@@ -18,21 +25,39 @@ export default function Header(props) {
                             <a className="nav-link" href="#">About</a>
                         </li>
                     </ul>
-                    {props.searchBar ? <form className="d-flex" role="search">
-                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                        <button className="btn btn-outline-success" type="submit">Search</button>
-                    </form> : ""}
+
+                    {props.searchBar && (
+                        <form className="d-flex me-3" role="search">
+                            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                            <button className="btn btn-outline-success" type="submit">Search</button>
+                        </form>
+                    )}
+
+                    {/* Theme Toggle */}
+                    <div className="form-check form-switch">
+                        <input
+                            className="form-check-input"
+                            type="checkbox"
+                            id="themeSwitch"
+                            checked={darkMode}
+                            onChange={toggleTheme}
+                        />
+                        <label className="form-check-label" htmlFor="themeSwitch">
+                            {darkMode ? "Dark Mode" : "Light Mode"}
+                        </label>
+                    </div>
                 </div>
             </div>
         </nav>
-    )
+    );
 }
 
 Header.defaultProps = {
     title: "Your Title Here",
     searchBar: true
-}
+};
+
 Header.propTypes = {
     title: PropTypes.string,
     searchBar: PropTypes.bool
-}
+};
