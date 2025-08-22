76163ec1c19778f6d74415a38776711a1ec7f45e
Kaamil1210 <kaamilgohel1210@gmail.com>
2025-08-22 22:30:50 +0530

    Update Addtodo.js

--- src/MyComponents/Addtodo.js
+++ src/MyComponents/Addtodo.js
@@ -12,11 +12,11 @@ export const Addtodo = (props) => {
             <form onSubmit={submit}>
                 <div class="mb-3">
                     <label for="title" class="form-label">Todo Title</label>
-                    <input type="text" class="form-control" id="title" aria-describedby="emailHelp" />
+                    <input type="text" value={title} onChange={(e)=>{setTitle(e.target.value)}}class="form-control" id="title" aria-describedby="emailHelp" />
                 </div>
                 <div class="mb-3">
                     <label for="desc" class="form-label">Todo Descreption</label>
-                    <input type="text" value={desc} class="form-control" id="desc" />
+                    <input type="text" value={desc} onChange={(e)=>{setDesc(e.target.value)}} class="form-control" id="desc" />
                 </div>
                 <button type="submit" class="btn btn-success btn-sm">Add Todo</button>
             </form>
