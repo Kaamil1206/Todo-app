7d9a79a31b5caf0039a720fe06914dafdd568463
Kaamil1210 <kaamilgohel1210@gmail.com>
2025-08-23 08:42:35 +0530

    Update Todos.js

--- src/MyComponents/Todos.js
+++ src/MyComponents/Todos.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import { Todoitem } from './Todoitem';
 export const Todos = (props) => {
+    let myStyle = {
+        minHeight: "70vh",
+        margin: "40px auto"
+    }
     return (
-        <div className='container'>
+        <div className='container' style={myStyle}>
             <h3 className=' my-3'>Todos Works</h3>
             {props.todos.length === 0 ? "No Todos to display" :
                 props.todos.map((todo) => {
