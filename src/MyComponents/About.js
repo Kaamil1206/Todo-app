02b0e62cc2d0442c45cca2c68020159b1ea2330d
Kaamil1210 <kaamilgohel1210@gmail.com>
2025-08-24 09:26:57 +0530

    Create About.js

---
+++ src/MyComponents/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            This is About page
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est nihil cupiditate cum adipisci vitae similique accusamus animi vel voluptatum rerum? Eos deleniti iure amet commodi consectetur, cupiditate laudantium dolorem accusamus.</p>
+        </div>
+    )
+}
