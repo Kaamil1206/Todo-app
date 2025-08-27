45cd08c5c3bcd5778018ac40e32adf06968c54fc
Kaamil1210 <kaamilgohel1210@gmail.com>
2025-08-27 08:17:45 +0530

    Update Todos.js

--- src/MyComponents/Todos.js
+++ src/MyComponents/Todos.js
@@ -1,19 +1,27 @@
 import React from 'react'
 import { Todoitem } from './Todoitem';
+
 export const Todos = (props) => {
     let myStyle = {
         minHeight: "70vh",
         margin: "40px auto"
-    }
+    };
+
     return (
         <div className='container' style={myStyle}>
-            <h3 className=' my-3'>Todos Works</h3>
-            {props.todos.length === 0 ? "No Todos to display" :
-                props.todos.map((todo) => {
-                    return <Todoitem todo={todo} key={todo.sno} onDelete={props.onDelete} />
+            <h3 className='my-3 text-center'>My Todos</h3>
+            {props.todos.length === 0
+                ? <p className="text-center text-muted">No Todos to display</p>
+                : props.todos.map((todo) => {
+                    return (
+                        <Todoitem
+                            todo={todo}
+                            key={todo.sno}
+                            onDelete={props.onDelete}
+                        />
+                    );
                 })
             }
-
         </div>
     )
 }
