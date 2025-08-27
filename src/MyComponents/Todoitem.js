528accf6bda2e71f5e884051236acc9c982991d4
Kaamil1210 <kaamilgohel1210@gmail.com>
2025-08-27 08:17:50 +0530

    Update Todoitem.js

--- src/MyComponents/Todoitem.js
+++ src/MyComponents/Todoitem.js
@@ -1,14 +1,18 @@
-import React from 'react'
+import React from 'react';
 
 export const Todoitem = ({ todo, onDelete }) => {
     return (
-        <>
-        <div>
-            <h4>{todo.title}</h4>
-            <p>{todo.desc}</p>
-            <button className="btn btn-sm btn-danger" onClick={() => { onDelete(todo) }}>Delete</button>
+        <div className="card shadow-sm mb-3">
+            <div className="card-body">
+                <h5 className="card-title">{todo.title}</h5>
+                <p className="card-text">{todo.desc}</p>
+                <button
+                    className="btn btn-danger btn-sm"
+                    onClick={() => onDelete(todo)}
+                >
+                    Delete
+                </button>
+            </div>
         </div>
-        <hr />
-        </>
-    )
-}
+    );
+};
