6cc3c51fc9148631e328114987c838d847e7f0f0
Kaamil1210 <kaamilgohel1210@gmail.com>
2025-08-27 09:51:53 +0530

    Update Todoitem.js

--- src/MyComponents/Todoitem.js
+++ src/MyComponents/Todoitem.js
@@ -1,13 +1,19 @@
 import React from 'react';
 
-export const Todoitem = ({ todo, onDelete }) => {
+export const Todoitem = ({ todo, onDelete, theme }) => {
     return (
-        <div className="card shadow-sm mb-3">
+        <div
+            className={`card shadow-sm mb-3 ${
+                theme === "dark" ? "bg-secondary text-light" : "bg-white text-dark"
+            }`}
+        >
             <div className="card-body">
                 <h5 className="card-title">{todo.title}</h5>
                 <p className="card-text">{todo.desc}</p>
                 <button
-                    className="btn btn-danger btn-sm"
+                    className={`btn btn-sm ${
+                        theme === "dark" ? "btn-outline-light" : "btn-danger"
+                    }`}
                     onClick={() => onDelete(todo)}
                 >
                     Delete
