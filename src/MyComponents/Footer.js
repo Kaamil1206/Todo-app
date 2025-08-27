22c7668a70416299e945a707a46c92434cb9cebb
Kaamil1210 <kaamilgohel1210@gmail.com>
2025-08-27 08:05:26 +0530

    Update Footer.js

--- src/MyComponents/Footer.js
+++ src/MyComponents/Footer.js
@@ -1,9 +1,8 @@
-import React from 'react'
 
 export const Footer = () => {
     
     return (
-        <footer className='bg-dark text-light text-center py-3' >
+        <footer className=' footer bg-dark text-light text-center py-3' style={{ position: "relative", bottom: "0", width: "100%", marginTop: "auto" }}>
             <p className="text-center">
                 Copyright &copy; MyTodosList.com | All Rights Reserved | Terms and Conditions
             </p>
