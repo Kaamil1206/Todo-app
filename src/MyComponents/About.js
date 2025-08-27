83addae2b32f0683869762cd82c0fdbab431c875
Kaamil1210 <kaamilgohel1210@gmail.com>
2025-08-27 08:05:40 +0530

    Update About.js

--- src/MyComponents/About.js
+++ src/MyComponents/About.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const About = () => {
     return (
-        <div>
+        <div className='container'>
             This is About page
             <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est nihil cupiditate cum adipisci vitae similique accusamus animi vel voluptatum rerum? Eos deleniti iure amet commodi consectetur, cupiditate laudantium dolorem accusamus.</p>
         </div>
