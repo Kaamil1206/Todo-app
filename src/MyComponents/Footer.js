40acebd614e91cb81b72afc8be7e9d11b0d696a5
Kaamil1210 <kaamilgohel1210@gmail.com>
2025-08-29 16:24:40 +0530

    Update Footer.js

--- src/MyComponents/Footer.js
+++ src/MyComponents/Footer.js
@@ -1,12 +1,23 @@
+import React from 'react';
 
+/**
+ * Footer Component
+ * Displays the copyright notice at the bottom of the page.
+ */
 export const Footer = () => {
-    
     return (
-        <footer className=' footer bg-dark text-light text-center py-3' style={{ position: "relative", bottom: "0", width: "100%", marginTop: "auto" }}>
+        <footer
+            className="footer bg-dark text-light text-center py-3"
+            style={{
+                position: "relative",  // Keeps footer at the bottom of the container
+                bottom: "0",
+                width: "100%",
+                marginTop: "auto"      // Pushes footer down if container grows
+            }}
+        >
             <p className="text-center">
-                Copyright &copy; MyTodosList.com | All Rights Reserved | Terms and Conditions
+                &copy; {new Date().getFullYear()} MyTodosList.com | All Rights Reserved | Terms and Conditions
             </p>
-
-        </footer >
-    )
-}
+        </footer>
+    );
+};
