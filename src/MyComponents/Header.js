eaf82d211c5245ef09c0b0be50ca2adc4957295f
Kaamil1210 <kaamilgohel1210@gmail.com>
2025-08-29 16:24:56 +0530

    Update Header.js

--- src/MyComponents/Header.js
+++ src/MyComponents/Header.js
@@ -2,14 +2,34 @@ import './Header.css';
 import PropTypes from 'prop-types';
 import { Link } from "react-router-dom";
 
-export default function Header(props) {
+/**
+ * Header Component
+ * Displays the navigation bar with links and an optional search bar.
+ */
+export default function Header({ title, searchBar }) {
     return (
         <nav className="navbar navbar-expand-lg glass-navbar">
             <div className="container-fluid">
-                <Link className="navbar-brand" to="/">{props.title}</Link>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent">
+
+                {/* Brand / App Title */}
+                <Link className="navbar-brand" to="/">
+                    {title}
+                </Link>
+
+                {/* Hamburger menu for mobile */}
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
                     <span className="navbar-toggler-icon"></span>
                 </button>
+
+                {/* Navbar links and optional search bar */}
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
@@ -19,23 +39,34 @@ export default function Header(props) {
                             <Link className="nav-link" to="/about">About</Link>
                         </li>
                     </ul>
-                    {props.searchBar ? (
+
+                    {/* Conditional Search Bar */}
+                    {searchBar && (
                         <form className="d-flex" role="search">
-                            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                            <button className="btn btn-outline-success" type="submit">Search</button>
+                            <input
+                                className="form-control me-2"
+                                type="search"
+                                placeholder="Search"
+                                aria-label="Search"
+                            />
+                            <button className="btn btn-outline-success" type="submit">
+                                Search
+                            </button>
                         </form>
-                    ) : null}
+                    )}
                 </div>
             </div>
         </nav>
     );
 }
 
+// Default props in case none are provided
 Header.defaultProps = {
     title: "Your Title Here",
     searchBar: true
 };
 
+// Type checking for props
 Header.propTypes = {
     title: PropTypes.string,
     searchBar: PropTypes.bool
